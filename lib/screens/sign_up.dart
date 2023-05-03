7051d010065c0c52ff45e9b915b4e013a5768548
Anurag <anusrivas07@gmail.com>
2023-05-03 21:18:14 +0530

    "app_food Project"

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -19,6 +19,27 @@ class SignUp extends StatelessWidget {
       ),
     );
   }
+
+  Widget button({required String buttonName, required Color color}){
+    return Container(
+      width:120.0,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
+          foregroundColor: MaterialStateProperty.all<Color>(color),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+              // side: BorderSide(color: Colors.red)
+            ),
+          ),
+        ),
+        onPressed: (){
+
+        }, child: Text(buttonName,style: TextStyle(fontSize:20.0, ),),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,34 +52,25 @@ class SignUp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-                Text('SignUp',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 40.0),),
-                Column(
-                  children: [
-                    textFeild(hinText: 'Name'),
-                    textFeild(hinText: 'UserName'),
-                    textFeild(hinText: 'Password'),
-                    textFeild(hinText: 'Confirm Password'),
-                  ],
+                Text('Sign Up',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 40.0),),
+                Container(
+                  height:300.0 ,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      textFeild(hinText: 'Name'),
+                      textFeild(hinText: 'UserName'),
+                      textFeild(hinText: 'Password'),
+                      textFeild(hinText: 'Confirm Password'),
+                    ],
+                  ),
                 ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    width:50.0,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(15)),
-                        foregroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0),
-                            // side: BorderSide(color: Colors.red)
-                          ),
-                        ),
-                      ),
-                     onPressed: (){
-
-                     }, child: Text(''),),
-                  ),
+                    button(buttonName: 'Cancel', color: Colors.grey),
+                    SizedBox(width: 10.0,),
+                  button(buttonName: 'Register', color: Colors.red)
                 ],
               )
             ],
